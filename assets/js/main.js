01f9ab6a5279fd2cfbd328a6adfd27d447c7be3f
vimalpksh <itisvimal@gmail.com>
2024-04-24 17:14:43 +0530

    header and nav bar almost done
    Need to check nav bar alignment alone and started working on the home section for now

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,38 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const novToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
+// MENU SHOW
+if (novToggle) {
+  novToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// MENU HIDE
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
-
 /*=============== EMAIL JS ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
